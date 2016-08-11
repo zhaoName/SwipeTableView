beed976b85e058af51a09a173df469b97823539b
zhaoName <zhao1529835@126.com>
2016-08-11 15:45:35 +0800

    Initial Commit

---
+++ SwipeTableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SwipeTableView
+//
+//  Created by zhao on 16/8/11.
+//  Copyright © 2016年 zhaoName. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SwipeTableView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SwipeTableView
+//
+//  Created by zhao on 16/8/11.
+//  Copyright © 2016年 zhaoName. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
